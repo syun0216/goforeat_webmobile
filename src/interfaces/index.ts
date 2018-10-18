92f274be29cfdd03a655113f8b17fb1eb8d10a95
syun <517897442@qq.com>
2018-10-18 09:33:37 +0800

    add foodlist interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,5 +1,5 @@
 import { HomePageMobx } from '../mobx/rootStore';
-
+import { FoodListMobx } from '../mobx/rootStore';
 
 
 /**
@@ -9,4 +9,8 @@ import { HomePageMobx } from '../mobx/rootStore';
 //homepage
 export interface IHomePage {
   homePageMobx: HomePageMobx
+}
+
+export interface IFoodList {
+  foodListMobx: FoodListMobx
 }
