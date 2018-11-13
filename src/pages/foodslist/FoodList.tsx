5ee2a1606d1a6edc6ca21616f2434b0c07d773ef
谭家杰 <tanjiajie@tanjiajiedeMacBook-Pro.local>
2018-11-14 01:18:35 +0800

    added login entrance on the top of menu

--- src/pages/foodslist/FoodList.tsx
+++ src/pages/foodslist/FoodList.tsx
@@ -21,8 +21,9 @@ const order = require("@/assets/order.png");
 const paytype = require("@/assets/payment.png");
 const setting = require("@/assets/setting.png");
 const topLogo = require("@/assets/logoTop.png");
+const avatar = require("@/assets/avatar.png");
 
-const COMPONENT_HEIGHT: number = document.documentElement.clientHeight;
+const COMPONENT_HEIGHT: number = document.documentElement!.clientHeight;
 
 @inject("basicMobx")
 @inject("foodListMobx")
@@ -120,7 +121,20 @@ export default class FoodList extends React.Component<IFoodList, {}> {
     return (
       <div>
         <div className="sidebar-top">
-          {GenerateIcon(topLogo, "topLogo", "sidebar-top-img")}
+          {GenerateIcon(avatar, "avatar", "sidebar-top-img")}
+          <div className="sidebar-top-text-container">
+            <span className="sidebar-top-text biggerFont">
+              日日有得食
+            </span>
+            <Link to="/login">
+              <span className="sidebar-top-text">
+                立即登錄
+              </span>
+            </Link>
+          </div>
+          <div className="sidebar-top-login">
+            <i className="fas fa-angle-right sidebar-top-login-arrow" />
+          </div>
         </div>
         {_drawerList.map((item, key) => (
           <List.Item key={key} multipleLine thumb={item.icon}>
@@ -129,6 +143,18 @@ export default class FoodList extends React.Component<IFoodList, {}> {
         ))}
       </div>
     );
+  }
+
+  private _goToLogin() {
+    return (
+      <Link to="/login">
+        <div>
+          <span>
+            1
+          </span>
+        </div>
+      </Link>
+    )
   }
 
   private _renderFoodListItem(rowData: IFoodListItem,sectionID: number, rowID: number) {
