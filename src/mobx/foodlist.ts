4e0b57b1515c84fb0b4f5d7fe65933444b77faf5
syun <517897442@qq.com>
2018-11-14 18:06:59 +0800

    更新 store文件

--- src/mobx/foodlist.ts
+++ src/mobx/foodlist.ts
@@ -1,16 +1,18 @@
 import { observable, action, computed } from 'mobx';
 //api
-import { getFoodList, foodPlaces } from '../api/request';
+import { getFoodList, foodPlaces, queryList } from '../api/request';
 //api interface
-import { IFoodListItem, IPlaceList } from '../interfaces/server';
+import { IFoodListItem, IPlaceList, IQueryList } from '../interfaces/server';
 //antd
 import { ListView } from 'antd-mobile';
+
 
 export default class FoodListMobx {
   @observable public foodList = new ListView.DataSource({
     rowHasChanged: (row1:IFoodListItem, row2:IFoodListItem) => row1 !== row2
   });
   @observable public placeList: IPlaceList[];
+  @observable public queryList: IQueryList[];
   @observable public values = {
     currentPlace: {
       id: 10,
@@ -18,16 +20,23 @@ export default class FoodListMobx {
     },
     isDrawerShow: false,
     isPlaceMenuShow: false,
+    isQueryListShow: true
   }
 
   @action.bound
   public toggleDrawer():void {
+    console.log(this);
     this.values.isDrawerShow = !this.values.isDrawerShow;
   }
 
   @action.bound
   public togglePlaceMenu():void {
     this.values.isPlaceMenuShow = !this.values.isPlaceMenuShow;
+  }
+
+  @action.bound
+  public closeQueryList():void {
+    this.values.isQueryListShow = false;
   }
 
   //api
@@ -38,6 +47,18 @@ export default class FoodListMobx {
       this.placeList = data;
       this.values.currentPlace = data[0];
       this.getFoodList(0, data[0].id);
+    }catch(e) {
+      console.log(e);
+      throw e;
+    }
+  }
+
+  @action.bound
+  public async getQueryList() {
+    try {
+      const {data} = await queryList();
+      this.queryList = data;
+      this.values.isQueryListShow = true;
     }catch(e) {
       console.log(e);
     }
@@ -54,10 +75,10 @@ export default class FoodListMobx {
   public async getFoodList(offset: number=0, placeId: number) {
     try {
       const {data} = await getFoodList(offset, placeId);
-      console.log(data);
       this.foodList = this.foodList.cloneWithRows(data.list)
     } catch(e) {
       console.log(e);
+      throw e;
     }
   }
 }
