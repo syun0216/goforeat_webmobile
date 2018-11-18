8cbb5e55fd0dbd84ee35f488a3c4e804a339e6cb
syun <517897442@qq.com>
2018-11-18 22:19:12 +0800

    更新utils

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,40 +1,11 @@
-function getWeekDay(date: number) {
-  const DATE: Date = new Date(date);
-  let weekDay: string | null = null;
-  switch (DATE.getDay()) {
-    case 0:
-      weekDay = '星期日';
-      break;
-    case 1:
-      weekDay = '星期一';
-      break;
-    case 2:
-      weekDay = '星期二';
-      break;
-    case 3:
-      weekDay = '星期三';
-      break;
-    case 4:
-      weekDay = '星期四';
-      break;
-    case 5:
-      weekDay = '星期五';
-      break;
-    case 6:
-      weekDay = '星期六';
-      break;
-    default:
-      weekDay = '';
-  }
-  return weekDay;
+export function getDeviceInfo() {
+  const u = navigator.userAgent;
+  const isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
+  const isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端 
+  const val = isiOS ? 'ios' : isAndroid ? 'android' : 'none';
+  return val;
 }
 
-export function endDate(endTimestamp: number) {
-  const ENDDATE: Date = new Date(endTimestamp);
-  let endDateFormat = '截止日期';
-  endDateFormat += `${ENDDATE.getHours()}:${ENDDATE.getMinutes()<10?`0${ENDDATE.getMinutes()}`:`${ENDDATE.getMinutes()}`} ${ENDDATE.getMonth()+1}月${ENDDATE.getDate()}日 ${getWeekDay(ENDDATE.getDay())}`;
-  return endDateFormat;
-}
 
 export function isEmpty(v: any) {
   switch (typeof v) {
