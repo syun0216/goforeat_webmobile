17bce5a98da4950df48f5625ef9329660b80a372
syun <517897442@qq.com>
2018-11-20 23:16:36 +0800

    更新mobx

--- src/mobx/foodlist.ts
+++ src/mobx/foodlist.ts
@@ -1,4 +1,4 @@
-import { observable, action } from 'mobx';
+import { observable, action, runInAction, decorate, configure } from 'mobx';
 //api
 import { getFoodList, foodPlaces, queryList } from '../api/request';
 //api interface
@@ -6,61 +6,73 @@ import { IFoodListItem, IPlaceList, IQueryList } from '../interfaces/server';
 //antd
 import { ListView } from 'antd-mobile';
 
-
-export default class FoodListMobx {
-  @observable public placeList: IPlaceList[];
-  @observable public queryList: IQueryList[];
-  @observable public values = {
+// configure({enforceActions: "observed"})
+class FoodListMobx {
+  public placeList: IPlaceList[];
+  public queryList: IQueryList[];
+  public values = {
     currentPlace: {
       id: 1,
-      name: '未选择'
+      name: '未选择'  
     },
     isDrawerShow: false,
     isPlaceMenuShow: false,
     isQueryListShow: true
   }
 
-  @action.bound
   public toggleDrawer():void {
     this.values.isDrawerShow = !this.values.isDrawerShow;
   }
 
-  @action.bound
   public togglePlaceMenu():void {
     this.values.isPlaceMenuShow = !this.values.isPlaceMenuShow;
   }
 
-  @action.bound
   public closeQueryList():void {
     this.values.isQueryListShow = false;
   }
 
   //api
-  @action.bound
   public async getFoodPlaces() {
     try{
       const {data} = await foodPlaces();
-      this.placeList = data;
-      this.values.currentPlace = data[0];
+      runInAction(() => {
+        this.placeList = data;
+        this.values.currentPlace = data[0];
+      })
     }catch(e) {
       console.log(e);
     }
   }
 
-  @action.bound
   public async getQueryList() {
     try {
       const {data} = await queryList();
-      this.queryList = data;
-      this.values.isQueryListShow = true;
+      runInAction(() => {
+        this.queryList = data;
+        this.values.isQueryListShow = true;
+      });
     }catch(e) {
       console.log(e);
     }
   }
 
-  @action.bound
   public changePlace(item: IPlaceList):void {
     this.values.currentPlace = item;
     this.togglePlaceMenu();
   }
 }
+
+decorate(FoodListMobx, {
+  placeList: observable,
+  queryList: observable,
+  values: observable,
+  toggleDrawer: action.bound,
+  togglePlaceMenu: action.bound,
+  closeQueryList: action.bound,
+  getFoodPlaces: action.bound,
+  getQueryList: action.bound,
+  changePlace: action.bound
+})
+
+export default FoodListMobx;
